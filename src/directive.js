82635a0ea17b41c4eb7a17368d9d151c9aa7a34a
Christopher Wong <cwong8257@gmail.com>
2019-11-13 23:02:18 -0500

    fix(directive.js): Fix cursor jumping to end position of input field when deleting a character

--- src/directive.js
+++ src/directive.js
@@ -8,47 +8,49 @@ function event (name) {
   return evt
 }
 
-export default function (el, binding) {
-  var config = binding.value || {}
-  if (Array.isArray(config) || typeof config === 'string') {
-    config = {
-      masked: true,
-      mask: config,
-      tokens: tokens
+export default {
+  bind (el, binding) {
+    var config = binding.value || {}
+    if (Array.isArray(config) || typeof config === 'string') {
+      config = {
+        masked: true,
+        mask: config,
+        tokens: tokens
+      }
     }
-  }
 
-  if (el.tagName.toLocaleUpperCase() !== 'INPUT') {
-    var els = el.getElementsByTagName('input')
-    if (els.length !== 1) {
-      throw new Error("v-mask directive requires 1 input, found " + els.length)
-    } else {
-      el = els[0]
+    if (el.tagName.toLocaleUpperCase() !== 'INPUT') {
+      var els = el.getElementsByTagName('input')
+      if (els.length !== 1) {
+        throw new Error("v-mask directive requires 1 input, found " + els.length)
+      } else {
+        el = els[0]
+      }
     }
-  }
 
-  el.oninput = function (evt) {
-    if (!evt.isTrusted) return // avoid infinite loop
-    /* other properties to try to diferentiate InputEvent of Event (custom)
-    InputEvent (native)
-      cancelable: false
-      isTrusted: true
+    var newDisplay = masker(el.value, config.mask, config.masked, config.tokens)
+    if (newDisplay !== el.value) {
+      el.value = newDisplay
+      el.dispatchEvent(event('input'))
+    }
+  },
+  update (el, binding) {
+    var config = binding.value || {}
+    if (Array.isArray(config) || typeof config === 'string') {
+      config = {
+        masked: true,
+        mask: config,
+        tokens: tokens
+      }
+    }
 
-      composed: true
-      isComposing: false
-      which: 0
-
-    Event (custom)
-      cancelable: true
-      isTrusted: false
-    */
     // by default, keep cursor at same position as before the mask
     var position = el.selectionEnd
     // save the character just inserted
     var digit = el.value[position-1]
     el.value = masker(el.value, config.mask, config.masked, config.tokens)
     // if the digit was changed, increment position until find the digit again
-    while (position < el.value.length && el.value.charAt(position-1) !== digit) {
+    while (position < el.value.length && digit && el.value.charAt(position-1) !== digit) {
       position++
     }
     if (el === document.activeElement) {
@@ -57,12 +59,6 @@ export default function (el, binding) {
         el.setSelectionRange(position, position)
       }, 0)
     }
-    el.dispatchEvent(event('input'))
-  }
-
-  var newDisplay = masker(el.value, config.mask, config.masked, config.tokens)
-  if (newDisplay !== el.value) {
-    el.value = newDisplay
     el.dispatchEvent(event('input'))
   }
 }
